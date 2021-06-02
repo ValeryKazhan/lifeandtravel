96a15306d18939e4cffec5e82d325a1e8cf7a6b5
ValeryKazhan <wolera11@gmail.com>
2021-06-02 12:27:19 +0300

    cleaned up

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,6 @@ use App\Http\Controllers\BlogController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
-Route::get('/', [PagesController::class, 'index']);
-
-Route::get('/services', [PagesController::class, 'services']);
-
-Route::get('/about', function () {
-    return view ('pages.about');
-});
-
-Route::get('/users/{id}', function ($id) {
-    return 'this is er '.$id;
-});
-*/
 
 Route::get('/', [BlogController::class, 'index']);
 
@@ -36,7 +23,3 @@ Route::get('/blog', [BlogController::class, 'blogList']);
 Route::get('/blog/pages/{number}', function($number){
     return view ("blog.pages.$number");
 });
-
-/*Route::get('/', [PagesController::class, 'index']);
-Route::resource('/blog', PostsController::class);
-Auth::routes();*/
